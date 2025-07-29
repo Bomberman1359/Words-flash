84ac6a1b2e0513c25b137da72996402d5294b82f
Bomberman1359 <chunhweechoi@gmail.com>
2025-07-28 21:22:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let words = [];
+let index = 0;
+let interval = null;
+
+function start() {
+  const text = document.getElementById("textInput").value;
+  words = text.trim().split(/\s+/);
+  index = 0;
+
+  const speed = parseInt(document.getElementById("speedInput").value);
+
+  if (interval) clearInterval(interval);
+
+  interval = setInterval(() => {
+    if (index < words.length) {
+      document.getElementById("wordDisplay").innerText = words[index];
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, speed);
+}
+
+function pause() {
+  if (interval) clearInterval(interval);
+}
 
